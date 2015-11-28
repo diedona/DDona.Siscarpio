727907cbfd47109c8be51de5f9231aa0b6cb7919
diedona <diedona@gmail.com>
2015-11-28 02:03:41 -0200

    acessando o serviço

---
+++ DDona.Siscarpio.ConsoleTeste/Program.cs
@@ -0,0 +1,25 @@
+﻿using DDona.Siscarpio.ConsoleTeste.UsuarioReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDona.Siscarpio.ConsoleTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UsuarioClient UsuarioClient = new UsuarioClient())
+            {
+                UsuarioListagemDTO[] Usuarios = UsuarioClient.GetAllUsuarios();
+
+                foreach (var Usuario in Usuarios)
+                {
+                    Console.WriteLine("{0}) Usuário '{1}' ('{2}')", Usuario.Id, Usuario.Nome, Usuario.Username);
+                }
+            }
+        }
+    }
+}
